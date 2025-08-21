e426310b60f2533f25d466b614930dd9435d9f8a
yliu-fort <liu.yuxu@husky.neu.edu>
2025-08-22 00:25:55 +0800

    Update init_aws_ami.sh

--- scripts/init_aws_ami.sh
+++ scripts/init_aws_ami.sh
@@ -2,10 +2,25 @@
 set -euo pipefail
 
 # === Disk Management ===
-#sudo mkfs -t ext4 /dev/nvme2n1
-#sudo mkdir /data
-#sudo mount /dev/nvme2n1 /data
-#sudo chown ubuntu:ubuntu /data
+DEVICE="/dev/nvme2n1"
+MOUNT_POINT="/data"
+
+# Check the file system type
+FSTYPE=$(lsblk -no FSTYPE "$DEVICE")
+
+if [ "$FSTYPE" != "ext4" ]; then
+  echo "[$DEVICE] is not ext4, start disk initialisation..."
+  sudo mkfs -t ext4 -F "$DEVICE"
+else
+  echo "[$DEVICE] is ext4, skip initialisation."
+fi
+
+# Create mount point and mount the disk
+sudo mkdir -p "$MOUNT_POINT" || true
+sudo mount "$DEVICE" "$MOUNT_POINT" || true
+
+# override the user permission
+sudo chown ubuntu:ubuntu "$MOUNT_POINT"
 
 # ===  ===
 PY_VER="3.10"
@@ -25,8 +40,19 @@ pip install --upgrade pip
 #pip install --index-url https://download.pytorch.org/whl/cu121 torch torchvision torchaudio
 
 echo "[*] Cloning repo..."
+# Backup runs if it exists
+if [ -d "$REPO_DIR/runs" ]; then
+  cp -r "$REPO_DIR/runs" /data/runs
+fi
+
+# Remove old repo and clone fresh
 rm -rf "$REPO_DIR"
 git clone "$REPO_URL" "$REPO_DIR"
+
+# Restore runs if backup exists
+if [ -d "/data/runs" ]; then
+  cp -r /data/runs "$REPO_DIR/runs"
+fi
 cd "$REPO_DIR" || { echo " : $REPO_DIR"; exit 1; }
 
 echo "[*] Installing project requirements..."
@@ -45,4 +71,10 @@ echo "To start working:"
 echo "cd $REPO_DIR"
 echo "source /opt/pytorch/bin/activate"
 echo "PYTHONPATH="$PWD/src" python src/diffusion_sandbox/train.py --config configs/cifar10_uncond.yaml"
-echo "tensorboard --logdir runs"
+echo "nohup env PYTHONPATH=/data/DDPM-minimal/src python src/diffusion_sandbox/train.py --config configs/cifar10_uncond.yaml > log.out 2>&1 &"
+echo "nohup tensorboard --logdir runs --host 0.0.0.0 --port 6006 > tb.out 2>&1 &"
+
+cd $REPO_DIR
+source /opt/pytorch/bin/activate
+nohup env PYTHONPATH="$PWD/src" python src/diffusion_sandbox/train.py --config configs/cifar10_uncond.yaml > log.out 2>&1 &
+nohup tensorboard --logdir runs --host 0.0.0.0 --port 6006 > tb.out 2>&1 &
